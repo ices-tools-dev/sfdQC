c17e2feb375b965d7286f9da99aafcb85f7e965d
Colin Millar <colin@ices.dk>
2018-06-15 13:32:55 +0100

    add benthis parameter table as dataset

--- data-raw/data_tblAux_benthisGearWidthsForWGSFD17.R
+++ data-raw/data_tblAux_benthisGearWidthsForWGSFD17.R
@@ -7,25 +7,20 @@ library(jsonlite)
 # settings
 config <- read_json("wgsfd_2018_dataset/config.json", simplifyVector = TRUE)
 
-# utilities
-source("QC/utilities.R")
-
-# create directories
-mkdir(config$data_dir)
-
 # connect to DB
 conn <- odbcDriverConnect(connection = config$db_connection)
 
-msg("downloading  ... ")
-  
 # set up sql command
 sqlq <- "select * from tblAux_benthisGearWidthsForWGSFD17"
 
 # fetch
-out <- sqlQuery(conn, sqlq)
-
-# save to file
-write.csv(out, file = paste0(config$data_dir, "/tblAux_benthisGearWidthsForWGSFD17.csv"), row.names = FALSE)
+benthis <- sqlQuery(conn, sqlq)
 
 # disconnect
 odbcClose(conn)
+
+# filter
+benthis <- benthis[!is.na(benthis$av_kw),]
+
+# add to data folder
+devtools::use_data(benthis, overwrite = TRUE)
