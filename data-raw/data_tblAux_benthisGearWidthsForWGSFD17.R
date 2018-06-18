5ac8aa4aa1c6ee94d90d5e188149793c0aae5a95
Colin Millar <colin@ices.dk>
2018-06-18 11:50:37 +0200

    implements: dplyr-ize database access #1
    Co-Authored-By: Einar Hjorleifsson <einarhjorleifsson@users.noreply.github.com>

--- data-raw/data_tblAux_benthisGearWidthsForWGSFD17.R
+++ data-raw/data_tblAux_benthisGearWidthsForWGSFD17.R
@@ -1,26 +1,28 @@
 
 # packages
-library(RODBC)
+library(DBI)
+library(odbc)
 library(icesTAF)
 library(jsonlite)
 
 # settings
-config <- read_json("wgsfd_2018_dataset/config.json", simplifyVector = TRUE)
+config <- read_json("data-raw/config.json", simplifyVector = TRUE)
 
 # connect to DB
-conn <- odbcDriverConnect(connection = config$db_connection)
-
-# set up sql command
-sqlq <- "select * from tblAux_benthisGearWidthsForWGSFD17"
+conn <- dbConnect(odbc::odbc(),
+                 driver = config$odbc$driver,
+                 server = config$odbc$server,
+                 Trusted_Connection = config$odbc$Trusted_Connection,
+                 database = config$odbc$database)
 
 # fetch
-benthis <- sqlQuery(conn, sqlq)
+benthis <-
+  dplyr::tbl(conn, "tblAux_benthisGearWidthsForWGSFD17") %>%
+  dplyr::filter(!is.na(av_kw)) %>%
+  collect(n = Inf)
 
 # disconnect
 odbcClose(conn)
-
-# filter
-benthis <- benthis[!is.na(benthis$av_kw),]
 
 # add to data folder
 devtools::use_data(benthis, overwrite = TRUE)
