1275477584d150525bbecf35b29144d7d26927d0
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-06-20 15:08:34 +0000

    spatialplot uses maps from maps-package

--- R/spatialplot.R
+++ R/spatialplot.R
@@ -1,13 +1,16 @@
 #' @export
 spatialplot <- function(data, xyrange = spatCore) {
-  ggplot(polLand, aes(long, lat)) +
+
+  map_data("world",
+           xlim = xyrange$xrange,
+           ylim = xyrange$yrange) %>%
+    ggplot(aes(long, lat)) +
     geom_polygon(aes(group=group), fill = "light grey") +
     coord_quickmap(xlim = xyrange$xrange,
                    ylim = xyrange$yrange) +
     labs(x = "Longitude", y = "Latitude") +
     geom_polygon(data = data,
                  aes(long, lat, group = group, fill = cols)) +
-    geom_polygon(data = polLand,
-                 aes(long, lat, group = group),
-                 colour = "black", size = 0.25, fill = "transparent")
+    geom_path(aes(long, lat, group = group),
+              colour = "black", size = 0.25)
 }
